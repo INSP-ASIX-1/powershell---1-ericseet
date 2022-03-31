6d3ab479e5fcf419def21bb56cc076dc2efcb3fb
ericseet <102758718+ericseet@users.noreply.github.com>
2022-03-31 20:23:34 +0200

    Update 05.ps1

--- 05.ps1
+++ 05.ps1
@@ -1,2 +1,4 @@
 #Mostra Id del procés que té per nom 'bash'
 #Si us plau aquest executa'l en PowerShell Core a Ubuntu
+$a=Get-Process -ProcessName bash | Select-Object -ExpandProperty Id
+Write-Output $a
